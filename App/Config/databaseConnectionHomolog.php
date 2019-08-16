de480a51f844f9716abfe2b86b4c138d83d3ea27
Junior Nascimento <juniorjr10@gmail.com>
2019-08-16 11:33:58 -0300

    Teste homolog

--- App/Config/databaseConnectionHomolog.php
+++ App/Config/databaseConnectionHomolog.php
@@ -1,6 +1,6 @@
 <?php 
-    $username = 'apisulamerica';
-    $password = 'Kodb!89Po';
+    $username = 'usr_fmoreira';
+    $password = 'Nsdb!sj948';
 
     try {
         $conn = new PDO('mysql:host=172.16.1.8;dbname=gsc;charset=utf8', $username, $password); # Homologacao
